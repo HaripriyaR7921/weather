814a46ac6985e9b89c49d9549ddaf26725d77e3c
Haripriya R <59635419+HaripriyaR7921@users.noreply.github.com>
2022-06-17 02:26:59 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,48 +1,62 @@
-$(document).ready(function(){
 
+const API_KEY ='49cc8c821cd2aff9af04c9f98c36eb74';
 
-
-    function info(value){
-        $.ajax({
-            url: "https://api.openweathermap.org/data/2.5/forecast?q="+value+"&appid=32ba0bfed592484379e51106cef3f204",
-            jsonpCallback: "callback",
-            dataType: "json",
-            success: function(data) {
-                    var min=0;
-                    var max=0
-                    var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-                    document.getElementById("cardview").innerHTML="";
-                    for(i = 0; i<7; i++){
-                        min =Number(data.list[i].main.temp_min - 273.15).toFixed(1);
-                        max = Number(data.list[i].main.temp_max - 273.15).toFixed(2);
-                        document.getElementById("cardview").innerHTML+=`
-                        <div class="card card-5">
-                                <h2 class="card__title">${weekday[i]}</h2>
-                                <p class="card__apply">
-                            MIN ${min}C
-                            </p>  <p class="card__apply">
-                            Max ${max}C
-                            </p>
-                      </div>
-                        `
-                    }
-                }
-          });
-    }
-    $("#location").on("keyup", function() {
-        info(this.value)
-    });
-    const API_KEY ='92fc5280eb2d26a5ecdd1d0a872b5d0b';
-    getWeatherData ()
-    function getWeatherData () {
-        navigator.geolocation.getCurrentPosition((success) => {
+  
+function updatedisplay(latitude,longitude){
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly&appid=${API_KEY}`).then(res => res.json()).then(data => {
+        var presentdiv = document.getElementById("current-temp")
+        var divcontainer = document.getElementById("weather-forecast")
+        presentdiv.innerHTML=""
+        divcontainer.innerHTML=""
+        console.log(data)
+        const d = new Date();
+        let cday = days[d.getDay()];
+        data.daily.forEach((day, idx) => {
             
-                var input = document.getElementById("location").value = "india"
-                info("india")
-        
-    
+            console.log(day)
+                if(idx == 0){
+                    presentdiv.innerHTML = `
+                    <div class="day">${cday}</div>
+                    <img src="http://openweathermap.org/img/wn//${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
+                    <div class="other">
+                        <div class="temp">Night - ${day.temp.night}&#176;C</div>
+                        <div class="temp">Day - ${day.temp.day}&#176;C</div>
+                    </div>
+                    `
+                }
+                if(idx<7 && cday!=days[idx]){
+                    divcontainer.innerHTML += `
+                    <div class="weather-forecast-item">
+                        <div class="day">${days[idx]}</div>
+                        <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                        <div class="temp">Night - ${day.temp.night}&#176;C</div>
+                        <div class="temp">Day - ${day.temp.day}&#176;C</div>
+                    </div>
+                    
+                    `
+                }
+            })
         })
-    }
+}
 
-  });
 
+function getWeatherData () {
+    var search = document.getElementById("loc")
+    
+        console.log(search.value);
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${search.value}&limit=1&appid=${API_KEY}`).then(res => res.json()).then(data => {
+            console.log(data,data[0].lat)
+            let latitude = data[0].lat
+            let longitude = data[0].lon
+            updatedisplay(latitude,longitude)
+        })
+
+}
+currentloc()
+function currentloc(){
+    navigator.geolocation.getCurrentPosition((success) => {
+        let {latitude, longitude } = success.coords;
+        updatedisplay(latitude,longitude)
+    })
+}
